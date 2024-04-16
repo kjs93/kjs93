a0b2cdc37c3f1b1e920415e8a2c1bf79d06c0b0c
kjs93 <skschwo93@gmail.com>
2024-04-16 17:04:51 +0900

    re4

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from naverfindmap import SeleniumConfig
+import time
+
 
 if __name__ == '__main__':
-    helper = SeleniumConfig()
-    helper.find_result('청량리역', '남부터미널')
-    helper.close_driver()
+    test = SeleniumConfig()
+    test.find_result('청량리역', '남부터미널')
+    time.sleep(5)
+    test.close_driver()
