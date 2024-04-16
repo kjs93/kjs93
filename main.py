067ac4b12032c373b7bcd70e598e7ccf1f244b45
kjs93 <skschwo93@gmail.com>
2024-04-16 16:55:05 +0900

    re2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from selenium_helper import SeleniumHelper
+from naverfindmap import SeleniumConfig
 
 if __name__ == '__main__':
-    helper = SeleniumHelper()
+    helper = SeleniumConfig()
     helper.navigate_and_search('청량리역', '남부터미널')
     helper.close_driver()
--- naverfindmap.py
+++ naverfindmap.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-class SeleniumHelper:
+class SeleniumConfig:
     def __init__(self):
         self.driver = webdriver.Chrome()
 
@@ -26,6 +26,7 @@ class SeleniumHelper:
             time.sleep(3)
             depart_send_keys = '/html/body/div[1]/div/div[2]/div[1]/div/div/div[1]/div/div[1]/div[1]/div[1]/div/div/div/input'
             arrive_send_keys = '/html/body/div[1]/div/div[2]/div[1]/div/div/div[1]/div/div[1]/div[1]/div[2]/div/div/div/input'
+            time_taken = '//*[@id="tab_pubtrans_directions"]/ul/li[1]/div/div/div/em/span[1]'
             self.send_text(By.XPATH, depart_send_keys, departure)
             time.sleep(3)
             self.find_element_click(By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div/div[1]/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[1]/ul/li[2]/div')
@@ -36,7 +37,6 @@ class SeleniumHelper:
             time.sleep(3)
             self.find_element_click(By.XPATH, '//*[@id="section_content"]/div/div[1]/div[2]/button[2]')
             time.sleep(5)
-            time_taken = '//*[@id="tab_pubtrans_directions"]/ul/li[1]/div/div/div/em/span[1]'
             print(f" 소요시간 : {self.find_element(By.XPATH, time_taken).get_attribute('innerText')} 분")
         except Exception as e:
             print("Error:", e)
